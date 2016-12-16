6a5492dd987b5f69cf6097e1c8f27f8b1bc8f530
PeachComment <PeachComment@gmail.com>
2016-12-16 23:14:57 +0100

    Start

---
+++ TicTacToe/src/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 16.12.2016
+ *      Author: PeachComment
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Tic Tac Toe" << endl;
+}
+
+
+
+
