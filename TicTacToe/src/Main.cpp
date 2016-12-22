b523a9aedab0c07053610b33c12667688e81435e
PeachComment <PeachComment@gmail.com>
2016-12-22 23:09:21 +0100

    Spieler-Eingabe einlesen und auswerten

--- TicTacToe/src/Main.cpp
+++ TicTacToe/src/Main.cpp
@@ -7,19 +7,54 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "Spielfeld.h"
 using namespace std;
 
+bool isFeldEingabe(string eingabe) {
+	if (eingabe.length() < 3) {
+		return false;
+	}
+	char c = eingabe.at(0);
+	if (c != '1' && c != '2' && c != '3') {
+		return false;
+	}
+	c = eingabe.at(1);
+	if (c != ' ') {
+		return false;
+	}
+	c = eingabe.at(2);
+	if (c != '1' && c != '2' && c != '3') {
+		return false;
+	}
+	return true;
+}
+
 int main() {
 
-	string eingabe;
+	Spielfeld* spielfeld = new Spielfeld();
+	spielfeld->zeichnen();
+
+	int spielerNr = 1;
+	string eingabe = "";
+	int zeile, spalte;
 
 	while (true) {
 
-		cin >> eingabe;
+		cout << endl;
+		cout << "Eingabe Spieler " << spielerNr << ": ";
+		getline(cin, eingabe);
 
-		if (eingabe == "start") {
-			Spielfeld* spielfeld = new Spielfeld();
+		if (isFeldEingabe(eingabe)) {
+			zeile = (eingabe.at(0) - '0') - 1;
+			spalte = (eingabe.at(2) - '0') - 1;
+			if (spielerNr == 1) {
+				spielfeld->setX(zeile, spalte);
+				spielerNr = 2;
+			} else {
+				spielfeld->setO(zeile, spalte);
+				spielerNr = 1;
+			}
 			spielfeld->zeichnen();
 		} else if (eingabe == "ende") {
 			exit(0);
